420e7874c5d39a75e5d33e6fca61ad22381626bb
Yoan Mollard <yoan.mollard@inria.fr>
2016-06-24 15:39:49 +0200

    Improved interaction

--- scripts/vocal_interactive_promps.py
+++ scripts/vocal_interactive_promps.py
@@ -9,11 +9,11 @@ import rospy
 rospy.init_node('vocal_interactive_promps')
 
 class VocalInteractiveProMPs(object):
-    def __init__(self):
+    def __init__(self, arm='left'):
         # MOTION
-        self.promp = InteractiveProMP('right')
-        self.right = ArmCommander('right')
-        self.init = self.right.get_current_state()
+        self.promp = InteractiveProMP(arm)
+        self.arm = ArmCommander(arm, ik='robot')
+        self.init = self.arm.get_current_state()
 
         # INTERACTION (TTS + Speech recognition -- Kinect)
         rospy.loginfo("setting up the kinect...")
@@ -40,7 +40,9 @@ class VocalInteractiveProMPs(object):
                         </rule>
                      </grammar>'''
         self.kinect.speech.params.set_grammar(grammar)
-        self.kinect.speech.start()  # start with no callback to use the get() method
+        self.kinect.display_speech()
+        success = self.kinect.speech.start()  # start with no callback to use the get() method
+        assert success == '', success
         rospy.loginfo("Kinect started!")
 
     def read_user_input(self, all_semantics):
@@ -53,57 +55,70 @@ class VocalInteractiveProMPs(object):
         return ""
 
     def record_motion(self):
-        self.kinect.tts.say('ready?')
-        for countdown in [3, 2, 1, "go"]:
+        for countdown in ['ready?', 3, 2, 1, "go"]:
+            self.say('{}'.format(countdown), blocking=False)
             rospy.sleep(1)
-            self.kinect.tts.say('{}'.format(countdown))
-        self.right.recorder.start()
-        
+        self.arm.recorder.start(10)
+        rospy.loginfo("Recording...")
+
         choice = ""
         while choice != 'stop':
             choice = self.read_user_input(['stop'])
-             
-        joints, eef = self.right.recorder.stop()
-        self.kinect.tts.say('Motion recorded')
+
+        joints, eef = self.arm.recorder.stop()
+        self.say('Motion recorded, please wait...', blocking=False)
         try:
             self.promp.add_demonstration(joints)
         except ValueError:
-            self.kinect.tts.say("Sorry I failed to record this demonstration")
+            self.say("Sorry I failed to record this demonstration")
 
     def set_goal(self):
         if self.promp.num_primitives > 0:
-            self.kinect.tts.say('Move the robot and say ready to set the goal')
+            self.say('Move the robot and say ready to set the goal')
 
             choice = ""
             while choice != 'ready':
                 choice = self.read_user_input(['ready'])
 
-            goal_set = self.promp.set_goal(self.right.endpoint_pose())
+            goal_set = self.promp.set_goal(self.arm.endpoint_pose())
             if goal_set:
-                self.kinect.tts.say('I can reach this point, let me demonstrate')
-                self.right.move_to_controlled(self.init)
+                self.say('I can reach this object, let me demonstrate', blocking=False)
+                self.arm.translate_to_cartesian([0, 0, 0.2], 'base', 2)
+                self.arm.move_to_controlled(self.init)
+                self.arm.open()
                 trajectory = self.promp.generate_trajectory()
-                self.right.execute(trajectory, epsilon=99999)
+                self.arm.execute(trajectory)
+                self.arm.close()
+                self.arm.translate_to_cartesian([0, 0, 0.2], 'base', 2)
+                if self.arm.gripping():
+                    rospy.loginfo('Take it!')
+                    rospy.sleep(3)
+                    self.arm.open()
             else:
-                self.kinect.tts.say("I can't reach this point. {}".format(self.promp.status_writing))
+                self.say("I don't know how to reach this object. {}".format(self.promp.status_writing))
         else:
-            self.kinect.tts.say('The is no demonstration yet, please record at least one demo')
+            self.say('The is no demonstration yet, please record at least one demo')
+
+    def say(self, what, blocking=False):
+        rospy.loginfo(what)
+        self.kinect.tts.say(what, True)
+        rospy.logwarn('BLOCKING')
 
     def run(self):
         while not rospy.is_shutdown():
-            self.right.move_to_controlled(self.init)
+            self.arm.move_to_controlled(self.init)
             if self.promp.num_primitives > 0:
-                self.kinect.tts.say('Do you want to record a motion or set a new goal?')
+                self.say('Do you want to record a motion or set a new goal?')
                 choice = self.read_user_input(['record', 'goal'])
                 if choice == 'record':
                     self.record_motion()
                 elif choice == 'goal':
                     self.set_goal()
             else:
-                self.kinect.tts.say("Let's record a first demo, say stop to finish")
+                self.say("Let's record a first demo, say stop to finish")
                 rospy.sleep(4)
                 self.record_motion()
-            self.kinect.tts.say('There are {} primitive{} and {} demonstration{}'.format(self.promp.num_primitives,
+            self.say('There are {} primitive{} and {} demonstration{}'.format(self.promp.num_primitives,
                                                                                          's' if self.promp.num_primitives > 0 else '', self.promp.num_demos,
                                                                                          's' if self.promp.num_demos > 0 else ''))
 

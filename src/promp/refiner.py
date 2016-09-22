d99c7ec6e1d8b2ef33d52ce5de73e36d0c2028d9
buschbapti <baptiste.busch@inria.fr>
2016-09-22 12:16:15 +0200

    Add the likelihood cost instead of jerk cost

--- src/promp/refiner.py
+++ src/promp/refiner.py
@@ -8,19 +8,20 @@ import numpy as np
 
 class RefiningCostFunction(CostFunction):
     """ CostFunction in which the distance to the goal and the task-space (or joint-space) jerk must be minimized."""
-    def __init__(self, arm, goal, num_basis, Gn, alpha, beta):
+    def __init__(self, arm, goal, mean, cov, num_basis, Gn, cost_factors=[]):
         self.goal = goal
-        self.alpha = alpha
-        self.beta = beta
+        self.cost_factors = cost_factors
         self.Gn = Gn
         self.fk = FK(arm)
         self.num_joints = len(self.fk.joints)
         self.num_basis = num_basis
+        self.mean = mean
+        self.cov = cov
 
     def weights_to_trajectories(self, sample):
         trajectory = []
         for joint in range(self.num_joints):
-            trajectory.append(np.dot(self.Gn, sample[joint*self.num_basis:(joint + 1) * self.num_basis]))
+            trajectory.append(np.dot(self.Gn, sample[joint * self.num_basis:(joint + 1) * self.num_basis]))
         return np.array(trajectory).T
 
     def cost_precision(self, trajectory):
@@ -36,22 +37,28 @@ class RefiningCostFunction(CostFunction):
         jerk = [np.absolute(np.diff(np.diff(np.diff(point)))) for point in cartesian_traj]
         return np.sum(jerk)
 
+    def cost_likelihood(self, sample):
+        # we remove the constant values from the cost and use -log of likelihood
+        mean_diff = sample - self.mean
+        return np.dot(mean_diff.T, np.solve(self.cov, mean_diff))
+
     def evaluate(self, sample):
         # Compute distance from sample to point
         trajectory = self.weights_to_trajectories(sample)
-        cost_jerk = self.cost_cartesian_jerk(trajectory)
+        # cost_jerk = self.cost_cartesian_jerk(trajectory)
         cost_precision = self.cost_precision(trajectory)
-        cost = self.alpha * cost_jerk + self.beta * cost_precision, cost_jerk, cost_precision
-        return cost
+        cost_likelihood = self.cost_likelihood(sample)
+        cost = self.cost_factors[0] * cost_likelihood + self.cost_factors[1] * cost_precision
+        return cost, cost_likelihood, cost_precision
 
 
 class TrajectoryRefiner(object):
-    def __init__(self, arm, num_basis, Gn, factor_jerk=1, factor_precision=1, n_samples_per_update=20, n_updates=100):
+    def __init__(self, arm, num_basis, Gn, factor_likelihood=1, factor_precision=1,
+                 n_samples_per_update=20, n_updates=100):
         self.arm = arm
         self.num_basis = num_basis
         self.Gn = Gn
-        self.factor_jerk = factor_jerk
-        self.factor_precision = factor_precision
+        self.cost_factors = [factor_likelihood, factor_precision]
         self.n_samples_per_update = n_samples_per_update
         self.n_updates = n_updates
 
@@ -69,10 +76,12 @@ class TrajectoryRefiner(object):
         weighting_method = 'PI-BB'
         covar_decay_factor = 0.8
         updater = UpdaterCovarDecay(eliteness, weighting_method, covar_decay_factor)
-        cost_function = RefiningCostFunction(self.arm, goal, self.num_basis, self.Gn, self.factor_jerk, self.factor_precision)
+        cost_function = RefiningCostFunction(self.arm, goal, self.num_basis, mean,
+                                             cov, self.Gn, self.cost_factors)
 
-        #import matplotlib.pyplot as plt
-        #fig = plt.figure(1, figsize=(15, 5))
+        # import matplotlib.pyplot as plt
+        # fig = plt.figure(1, figsize=(15, 5))
 
-        mean, cov = runOptimization(cost_function, distribution, updater, self.n_updates, self.n_samples_per_update)  #, fig, '/tmp/freek')
+        mean, cov = runOptimization(cost_function, distribution, updater,
+                                    self.n_updates, self.n_samples_per_update)  # ,fig, '/tmp/freek')
         return mean
